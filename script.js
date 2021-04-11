34b0df9a47d32af8618f0069b010fc83803fb39b
Glenn Ellis <glennaellis94@gmail.com>
2021-04-11 18:14:55 -0400

    fixed console error with if/else statement when no criteria is selected

--- script.js
+++ script.js
@@ -17,65 +17,66 @@ var generatePassword = function () {
 		var confirmUpperCase = window.confirm("Would you like to include Uppercase letters?");
 		var confirmNumbers = window.confirm("Would you like to include numbers?");
 		var confirmSpecialChar = window.confirm("Would you like to include special characters?");
-		//shows error if no character types are selected
+		//shows error message if no character types are selected
 		if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === false) {
 			window.alert("You must select atleast one character type to generate a password");
-		}
-		//logic to generate possible password characters from user input
-		if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === true) {
-			var passwordOptions = lowerCase + upperCase + Numbers + specialChar;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === false) {
-			var passwordOptions = lowerCase + upperCase + Numbers;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === true) {
-			var passwordOptions = lowerCase + upperCase + specialChar;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === true) {
-			var passwordOptions = lowerCase + Numbers + specialChar;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === true) {
-			var passwordOptions = upperCase + Numbers + specialChar;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === false) {
-			var passwordOptions = lowerCase + upperCase;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
-			var passwordOptions = lowerCase + specialChar;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === false) {
-			var passwordOptions = lowerCase + Numbers;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === false) {
-			var passwordOptions = upperCase + Numbers;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === true) {
-			var passwordOptions = upperCase + specialChar;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === true) {
-			var passwordOptions = Numbers + specialChar;
-		}
-		if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === false) {
-			var passwordOptions = lowerCase;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === false) {
-			var passwordOptions = upperCase;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === false) {
-			var passwordOptions = Numbers;
-		}
-		if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
-			var passwordOptions = specialChar;
-		}
-		console.log(passwordOptions);
+		} else {
+			//logic to generate possible password characters from user input
+			if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === true) {
+				var passwordOptions = lowerCase + upperCase + Numbers + specialChar;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === false) {
+				var passwordOptions = lowerCase + upperCase + Numbers;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === true) {
+				var passwordOptions = lowerCase + upperCase + specialChar;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === true) {
+				var passwordOptions = lowerCase + Numbers + specialChar;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === true) {
+				var passwordOptions = upperCase + Numbers + specialChar;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === false) {
+				var passwordOptions = lowerCase + upperCase;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
+				var passwordOptions = lowerCase + specialChar;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === false) {
+				var passwordOptions = lowerCase + Numbers;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === false) {
+				var passwordOptions = upperCase + Numbers;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === true) {
+				var passwordOptions = upperCase + specialChar;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === true) {
+				var passwordOptions = Numbers + specialChar;
+			}
+			if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === false) {
+				var passwordOptions = lowerCase;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === false) {
+				var passwordOptions = upperCase;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === false) {
+				var passwordOptions = Numbers;
+			}
+			if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
+				var passwordOptions = specialChar;
+			}
+			console.log(passwordOptions);
 
-		//create for loop to choose password characters
-		var password = "";
-		for (var i = 0; i < promptPasswordLength; i++) {
-			var password = password + passwordOptions.charAt(Math.floor(Math.random() * Math.floor(passwordOptions.length - 1)));
+			//create for loop to choose password characters
+			var password = "";
+			for (var i = 0; i < promptPasswordLength; i++) {
+				var password = password + passwordOptions.charAt(Math.floor(Math.random() * Math.floor(passwordOptions.length - 1)));
+			}
+			console.log(password);
+			return password;
 		}
-		console.log(password);
-		return password;
 	} else {
 		window.alert("Error! Your password length must be between 8 and 128 characters long and entered in as a numeric value.");
 	}
