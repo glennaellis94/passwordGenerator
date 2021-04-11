4a9d31346ad33846ca4c2b0cf523003c70324cd5
Glenn Ellis <glennaellis94@gmail.com>
2021-04-11 15:57:03 -0400

    added logic in for loop

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "|", "?", ">", "<", ":", ";"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var Numbers = ["0123456789"];
+var specialChar = ["!@#$%^&*()_+|?><:;"];
 
 var confirmLowerCase = window.confirm("Would you like to include lowercase letters?");
 var confirmUpperCase = window.confirm("Would you like to include uppercase letters?");
@@ -16,7 +16,7 @@ if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers ===
     var password=
   }*/
 }
-if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === flase) {
+if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === true && confirmSpecialChar === false) {
 	var passwordOptions = lowerCase + upperCase + Numbers;
 }
 if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === true) {
@@ -31,7 +31,7 @@ if (confirmLowerCase === false && confirmUpperCase === true && confirmNumbers ==
 if (confirmLowerCase === true && confirmUpperCase === true && confirmNumbers === false && confirmSpecialChar === false) {
 	var passwordOptions = lowerCase + upperCase;
 }
-if (confirmLowerCase === true && confirmUpperCase === flase && confirmNumbers === false && confirmSpecialChar === true) {
+if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
 	var passwordOptions = lowerCase + specialChar;
 }
 if (confirmLowerCase === true && confirmUpperCase === false && confirmNumbers === true && confirmSpecialChar === false) {
@@ -58,6 +58,16 @@ if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers =
 if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialChar === true) {
 	var passwordOptions = specialChar;
 }
+console.log(passwordOptions);
+var pass = function () {
+	//create for loop to choose password characters
+	for (var i = 0; i < promptPasswordLength; i++) {
+		password = password + passwordOptions.charAt(Math.floor(Math.random() * Math.floor(passwordOptions.length - 1)));
+	}
+	console.log(password);
+	window.alert(password);
+};
+pass();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
